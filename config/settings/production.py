7575a7b8021e23153e91e3df5e14de27acd5f222
meganmcgeee <meganpmcgee@gmail.com>
2018-06-21 12:47:12 -0700

    Added heroku ur to allowed hosts. Seeing if this works

--- config/settings/production.py
+++ config/settings/production.py
@@ -8,7 +8,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'pitchlist-mvp.herokuapp.com/']
 
 # DATABASES
 # ------------------------------------------------------------------------------
